c34b5c564ce7a6b5dda34b81dcd666438b324e8d
Filipe Ariel <filipe.arielp@gmail.com>
2021-09-29 21:28:19 -0300

    inicializando repositório de aulas js

---
+++ Aula 13/ambiente.js
@@ -0,0 +1,31 @@
+var c = 1
+do {
+    console.log (`Esse é o tudo bem número ${c}`)
+    c++ // = c = c+1 ou c += 1
+} while (c <= 6)    
+
+
+
+
+
+
+/*var c = 1
+while (c <= 500){
+    console.log (`Esse é o tudo bem número ${c}`)
+    c++ // = c = c+1 ou c += 1
+}*/
+
+
+
+
+
+
+
+
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')*/
